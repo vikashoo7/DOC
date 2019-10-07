d55371466ef917a766a8c4f14b5a14de7ed623ed
Vikash@example.com <root@git.example.com>
2019-10-07 12:21:22 -0400

    directory structiure

---
+++ Ansible/tar-zookeeper-installation/tar-zookeeper-installation/files/zookeeper-env.sh
@@ -0,0 +1,3 @@
+ZOO_LOG_DIR="/home/zookeeper/log"
+ZOO_LOG4J_PROP="INFO,ROLLINGFILE"
+SERVER_JVMFLAGS="-Xms2048m -Xmx2048m -verbose:gc -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -Xloggc:$ZOO_LOG_DIR/zookeeper_gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=9 -XX:GCLogFileSize=20M"
